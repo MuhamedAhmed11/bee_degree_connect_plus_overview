80e8dadd37596e1f3798901d4d6962bfd8a36674
MuhamedAhmed11 <mohamed.a.abdelmonem10@gmail.com>
2023-05-24 15:44:33 +0300

    Template file added

---
+++ main.dart
@@ -0,0 +1,6 @@
+// This is just a template file to allow GitHub to detect that this project done
+// with flutter because I can't share the code for confidential and security reasons
+
+var projectName = 'Bee Degree Connect Plus';
+var projectDescription =
+    'Mobile Application designed for children. It makes learning English more exciting and delightful through developing the main four skills of learning; listening, reading, speaking and writing.';
